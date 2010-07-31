b780122cb0674bde26ba9ae29137d47afe3134d5
Quan Sun <qsun@pardiff.com>
2010-07-31 22:33:16 +1000

    mockup, now it can work.

--- ffi.lisp
+++ ffi.lisp
@@ -10,3 +10,97 @@
     :string
   (url :string))
 ;; (encode-uri "a b")
+
+(defcstruct tailq-entry
+            (tqe-next :pointer)
+            (tqe-prev :pointer))
+
+(defctype size-t :unsigned-int)
+
+(defcstruct evhttp
+  (next tailq-entry)
+  (sockets tailq-entry)
+  (callback tailq-entry)
+  (connections tailq-entry)
+  (virtual-hosts tailq-entry)
+  (vhost-pattern :string)
+  (timeout :int)
+  (default-max-headers-size size-t)
+  (gencb :pointer)
+  (base :pointer))
+
+(defcfun ("event_base_new" event-base-new)
+    :pointer)
+
+(defcfun ("evhttp_new" event-http-new)
+    evhttp
+  (base :pointer))
+
+;; (setq *base* (event-base-new))
+;; (setq *myhttp* (event-http-new *base*))
+
+(defcfun ("evhttp_bind_socket" evhttp-bind-socket)
+    :int
+  (http evhttp)
+  (address :string)
+  (port :int))
+;; (evhttp-bind-socket *myhttp* "127.0.0.1" 8089)
+
+(defcfun ("event_base_dispatch" event-base-dispatch)
+    :int
+  (base :pointer))
+;; (event-base-dispatch *base*)
+
+(defcfun ("evbuffer_new" evbuffer-new)
+    :pointer)
+(defcfun ("evbuffer_free" evbuffer-free)
+    :void
+  (evb :pointer))
+;; (evbuffer-free (evbuffer-new))
+
+
+(defcfun ("evbuffer_add_printf" evbuffer-add-printf)
+    :void
+  (evb :pointer)
+  (text :string))
+(setq *evbuffer* (evbuffer-new))
+(evbuffer-add-printf *evbuffer* "test")
+
+(defcfun ("evhttp_send_reply" evhttp-send-reply)
+    :void
+    (req :pointer)
+    (code :int)
+    (reason :string)
+    (databuffer :pointer))
+
+(defcallback http-gen-cb-test 
+    :void
+    ((req :pointer)
+     (arg :pointer))
+  (evhttp-send-reply req 200 "everything" *evbuffer*))
+
+(defcfun ("evhttp_set_gencb" evhttp-set-gencb)
+    :void
+  (evhttp evhttp)
+  (callback-function :pointer)
+  (arg :pointer))
+;; (evhttp-set-gencb *myhttp* (callback http-gen-cb-test) (null-pointer))
+  
+
+;; void
+;; http_dispatcher_cb(struct evhttp_request *req, void *arg)
+;; {
+
+;;         struct evbuffer *evb = evbuffer_new();
+;;         event_debug(("%s: called\n", __func__));
+;;         evbuffer_add_printf(evb, "DISPATCHER_TEST");
+
+;;         evhttp_send_reply(req, HTTP_OK, "Everything is fine", evb);
+
+;;         evbuffer_free(evb);
+;; }
+
+
+
+
+  
